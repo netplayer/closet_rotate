d3cd178ae7d63584914ddecad1990a8bc71b627f
netplayer <netplayer@netplayer.gr>
2015-02-27 14:34:44 +0200

    initialize

--- closet_rotate/script.js
+++ closet_rotate/script.js
@@ -1,4 +1,57 @@
 
+$(document).ready(function(){ 
+    
+    //Get name and quantity of of items (in production "items" will be server-side-constructed array) 
+    var names=new Array('TopBrewer-Kabinet1.62','TopBrewer-Kabinet1.63');
+    var items=new Array('DarkWood','lightWood');
+    var url='http://netplayer.gr/demo22/'+names[0]+'/frames/'+items[0]+'.0.jpg';
+    window.console.log(url);
+    
+    //initialize
+    $('#main').attr('src',url);
+    $('#main').fadeIn('slow');
+    $('#color').val(names[0]);
+    $('#type').val(items[0]);
+    $('#frame').val(0);
+    for(i=0;i<names.length;i++){  
+        itemsName=names[i];        
+        itemsName=new Array();
+        for(x=0;x<25;x++){
+            itemsName.push(items[i]+'.'+x+'.jpg');
+            var img='<img id='+itemsName[x]+' src="http://netplayer.gr/demo22/'+names[i]+'/frames/'+itemsName[x]+'" />';
+            $('#preload').append(img);            
+            $('#frameNo').val(x);
+        };         
+    };    
+    //Costruct configuration color buttons  
+    for(i=0;i<items.length;i++){                      
+                          
+        $('#next').before("&nbsp;<a href='javascript:void(0);' class='option'  id="+names[i]+">"+items[i]+"</a>");
+                  
+    }; 
+
+    //give functionality to buttons
+
+    //1) color buttons
+    $('.option').click(function(){
+        var arr=new Array();
+        var itemcolor=$(this).attr('id');
+        for(i=0;i<names.length;i++){
+            if(names[i]==itemcolor){
+                var color=names[i]; 
+                var type=items[i];
+                var frame= parseInt($('#frame').val());          
+            }        
+        };    
+        var txt=type+'.'+frame+'.jpg';   
+        var isrc=document.getElementById(txt).src;
+        $('#frame').val(frame);
+        $('#main').attr('src',isrc);    
+   
+        $('#color').val(color);
+        $('#type').val(type);    
+    });
+});
 
 
 //Would be good to mousehold
